6c7958a58c4b014955696a813afa356fd3fe759c
Rashid Yangurazov <yv.rashid@outlook.com>
2021-11-04 15:39:14 +0300

    deleting a project

--- Dijkstra's algorithm/main.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-import graph as nw
-import classes as system_classes
-
-
-parent = Tk()
-g = nw.Graph() # создание графа
-w = system_classes.GUI(parent, g)
-parent.mainloop()
